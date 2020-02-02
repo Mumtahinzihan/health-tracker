755845f43fb418fea2d9dac10224537a6004cc8d
Mumtahinzihan <mumtahin@yahoo.com>
2020-02-02 21:20:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Water from './Water';
 
 function App() {
+  let [title, setTitle] = useState(localStorage.getItem('Mode'));
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('Mode', title);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>HEALTH TRACKER</h1>
+      <form type='submit'>
+        <h2>Please enter your Mode</h2>
+        <select className='modes'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option></option>
+          <option>I am Good</option>
+          <option>I am Normal</option>
+          <option>I am Crying</option>
+          <option>I am Worried</option>
+          <option>I am Feeling bad</option>
+          <option>I am Feeling Extreme bad</option>
+          <option>I need Ambulence</option>
+        </select>
+        <h4>Status: <em>{title}</em></h4>
+        <button onClick={e => handleSubmit(e)}>SEND</button>
+        <Water />
+      </form>
     </div>
   );
 }
---
+++ src/Water.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+export default function Water() {
+    let [water, setWater] = useState(0);
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('Water taken', water);
+        localStorage.getItem('Water taken', water);
+      }
+    return (
+        <div>
+            <h2>Please enter the amount of water you have taken in last 2 hours</h2>
+            <input className='water'
+                type='number'
+                value={water}
+                onChange={(e) => setWater(e.target.value)} />
+            <h4>{water >= 0 ? `You have taken ${water} glass of water`: 
+            `Enter a positive number`}</h4>
+            <h4>{water < 3 && water >= 0 ? 
+            `Water level is not enough. Please drink more ${3 - water} glasses of water` :
+             water > 7 ? 
+             `Too much water may dilute your body salt` :
+             water < 0 ? `Enter a positive number`:
+             `The water level is Perfect `}
+             </h4>
+             <button onClick={e => handleSubmit(e)}>SEND</button>
+        </div>
+    );
+}
